cb59563703416f87377a6bd7cd571d8bc20e1c16
Crimson <86634589+vickcrimson@users.noreply.github.com>
2021-12-22 22:23:05 -0300

    Projeto na metade do processo de conclusão
    *Programando a contagem em minutos
    *Melhorando o design

---
+++ brain.js
@@ -0,0 +1,50 @@
+window.onload = function(){
+
+    var seconds = 00;
+    var tens = 00;
+    var appendTens = document.getElementById("tens")
+    var appendSeconds = document.getElementById("seconds")
+    var buttonStart = document.getElementById("startButton");
+    var buttonStop = document.getElementById("pauseButton");
+    var markButton = document.getElementById("markButton");
+    var resetButton = document.getElementById("resetButton");
+    var Interval;
+
+buttonStart.onclick = function(){
+    clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+}
+
+buttonStop.onclick = function(){
+    clearInterval(Interval);
+}
+
+resetButton.onclick = function(){
+    clearInterval(Interval);
+    tens = "00";
+    seconds = "00";
+    appendTens.innerHTML = tens;
+    appendSeconds.innerHTML = seconds;
+}
+
+function startTimer(){
+    tens++;
+        if(tens <= 9 ){
+            appendTens.innerHTML = "0" + tens;
+        }
+        if(tens > 9){
+            appendTens.innerHTML = tens;
+        }
+        if(tens>99){
+            console.log("seconds");
+            seconds++;
+            appendSeconds.innerHTML = "0" + seconds;
+            tens = 0
+            appendTens.innerHTML="0"+ 0;
+        }
+        if (seconds>0){
+            appendSeconds.innerHTML = seconds;
+        }
+    }
+
+}
